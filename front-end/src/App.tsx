ae92bd6efd7888fd9c5aca69fa6d5e3e91c8097e
Roger <contatorooger@gmail.com>
2024-12-23 23:58:50 -0300

    [feat]: criando o arquivo de rotas

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,3 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
 export const App = () => {
-  return <main>app</main>;
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 };

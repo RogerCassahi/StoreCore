b1e044c5d4926a5017b0df950373f5f087b188a3
Roger <contatorooger@gmail.com>
2024-12-24 06:39:30 -0300

    [style] adicionado o arquivo global.scss

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles/_reset.scss";
+import "./styles/_global.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

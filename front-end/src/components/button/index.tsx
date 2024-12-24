666b23b8c0c12bffd683bdaeb35619e5ec82cf71
Roger <contatorooger@gmail.com>
2024-12-23 21:12:22 -0300

    [components] componente button

---
+++ front-end/src/components/button/index.tsx
@@ -0,0 +1,20 @@
+import { ButtonHTMLAttributes, ReactElement } from "react";
+import styles from "./styles.module.scss";
+
+type ButtonProps = {
+  button: ButtonHTMLAttributes<HTMLButtonElement>;
+  name: string;
+  prefix?: ReactElement;
+  suffix?: ReactElement;
+  type: "primary" | "secondary" | "disabled" | "danger" | "text";
+};
+
+export const Button = ({ button, prefix, suffix, name, type }: ButtonProps) => {
+  return (
+    <button {...button} className={styles[`button_${type}`]}>
+      {prefix && prefix}
+      {name}
+      {suffix && suffix}
+    </button>
+  );
+};
